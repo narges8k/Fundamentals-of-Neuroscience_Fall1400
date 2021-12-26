f947d72ae9514e63ed342e5b9161ff431c314c68
Narges <m.dabiri48@gmail.com>
2021-12-26 16:11:16 +0330

    minor update

--- PETCH_RasterPlot/RasterPlot.jl
+++ PETCH_RasterPlot/RasterPlot.jl
@@ -13,19 +13,19 @@ for i in 1:100
         end
     end
 end
-scatter()
+plot([0,0], [0, 100], color=:red)
 for i in 1: 100
     scatter!(spike_list[i],[i for j in 1: length(spike_list[i])], markershape=:vline)
 end
 plot!(title=L"Raster Plot", xlabel=L"Time\ (sec)",ylabel=L"The\ Test\ Number", legend=false, dpi=400)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/PETCH_RasterPlot/Figs/All_RasterPlot_plot.pdf")
 ####First Test
-scatter()
+plot(plot([0,0], [1, 2], color=:red))
 scatter!(spike_list[1],[1 for j in 1: length(spike_list[1])], markershape=:vline, markercolor=:black,
         xlabel=L"Time(sec)", ylabel=L"The\ Test\ Number", title=L"Raster\ Plot", label=L"The\ First\ Test",dpi=400)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/PETCH_RasterPlot/Figs/First_RasterPlot_plot.pdf")
 ####First 20 tests
-scatter()
+plot([0,0], [0, 20], color=:red)
 for i in 1: 20
     scatter!(spike_list[i],[i for j in 1: length(spike_list[i])], markershape=:vline)
 end

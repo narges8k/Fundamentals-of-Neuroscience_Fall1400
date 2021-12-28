1f29444886fb9e71854543a7d8e31dc53efc9541
Narges <m.dabiri48@gmail.com>
2021-12-28 22:47:23 +0330

    Update

--- Spike_Triggereⅾ _Average/code.jl
+++ Spike_Triggereⅾ _Average/code.jl
@@ -6,37 +6,32 @@ vars
 vars["Stim"]
 vars["Spike_times"]
 #######A
-scatter(vars["Stim"][1:2000], label= L"Stimulation\ Signal",
-    color=:red, ylabel=L"Signal's\ Value", xlabel=L"0<Time<1 (sec) \times 2000", dpi=600)
-plot(vars["Stim"][1:2000], title=L"Stimulus\ (First\ Sec)", ylabel=L"Magnitude", xlabel=L"Time,pts", legend=false)
+plot(collect(range(0.0, 1.0, length=2000)),vars["Stim"][1:2000], title=L"Stimulus\ (First\ Sec)", ylabel=L"Voltage(mV)", xlabel=L"Time(s)", legend=false)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/Spike_Triggereⅾ _Average/Figs/First_sec_signal_plot.pdf")
-savefig("../../Fundamentals-of-Neuroscience_Fall1400/Spike_Triggereⅾ _Average/Figs/First_sec_signal.pdf")
 
 #######B
 rand_spike=rand(vars["Spike_times"],20)
 plots=[]
 for i in rand_spike
+    y=vars["Stim"][floor(Int, (i-0.075)*2000):floor(Int, i*2000)]
     plt=begin
-        plot(vars["Stim"][floor(Int, (i-0.75)*2000):floor(Int, i*2000)],
-            title=L"Spike\ time\ :%$i",markersize=3,markerstrokewidth = 0.1)
+        plot(collect(range(i-0.075, i, length=length(y))),y, title=L"Spike\ time\ :%$i",markersize=3,markerstrokewidth = 0.1)
     end
     push!(plots, plt)
 end
-plot(plots...,size = (1000,1000), legend=false)
+plot(plots...,size = (1000,1000),xlabel=L"Time(s)", ylabel=L"Voltage(mV)", legend=false)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/Spike_Triggereⅾ _Average/Figs/20random_75msecBefore_plot.pdf")
-savefig("../../Fundamentals-of-Neuroscience_Fall1400/Spike_Triggereⅾ _Average/Figs/20random_75msecBefore.pdf")
 #######c
 spi_tri_list=[]
-for i in vars["Spike_times"][1:4]
-    push!(spi_tri_list, vars["Stim"][1:floor(Int, i*2000)])
-end
-for i in vars["Spike_times"][5:end]
-    push!(spi_tri_list, vars["Stim"][floor(Int, (i-0.75)*2000):floor(Int, i*2000)])
+for i in vars["Spike_times"]
+    push!(spi_tri_list, vars["Stim"][floor(Int, (i-0.075)*2000):floor(Int, i*2000)])
 end
 spi_tri_avg=[]
 for i in spi_tri_list
     push!(spi_tri_avg, mean(i))
 end
 spi_tri_avg
-plot(range(;length = 598, start = 1, stop = 90),spi_tri_avg)
+plot(spi_tri_avg)
+#######D
+
 close(file)

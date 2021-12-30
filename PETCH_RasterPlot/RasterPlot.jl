68e12a960eed3c1206ba3016128942592c948000
Narges <m.dabiri48@gmail.com>
2021-12-30 12:09:58 +0330

    spike time is set for zero on x-axis

--- PETCH_RasterPlot/RasterPlot.jl
+++ PETCH_RasterPlot/RasterPlot.jl
@@ -7,26 +7,26 @@ spike_list=[[] for i in 1:100]
 for i in 1:100
     for j in 1:500
         if vars["trials"][i,j]==1
-            push!(spike_list[i], j)
+            push!(spike_list[i], j/2000 - 0.05)
         end
     end
 end
-plot([0,0], [0, 100], color=:red)
+plot([0,0], [0, 100], color=:red, label="Spike Time")
 for i in 1: 100
-    scatter!(spike_list[i],[i for j in 1: length(spike_list[i])], markershape=:vline)
+    scatter!(spike_list[i],[i for j in 1: length(spike_list[i])], markershape=:vline, label=nothing)
 end
-plot!(title=L"Raster Plot", xlabel=L"Time\ (sec)",ylabel=L"The\ Test\ Number", legend=false, dpi=400)
+plot!(title=L"Raster\ Plot\ For\ All\ Trials", xlabel=L"Time\ (sec)",ylabel=L"The\ Test\ Number", label=nothing, dpi=400)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/PETCH_RasterPlot/Figs/All_RasterPlot_plot.pdf")
 ####First Test
-plot(plot([0,0], [1, 2], color=:red))
+plot(plot([0,0], [0, 2], color=:red, label="Spike Time"))
 scatter!(spike_list[1],[1 for j in 1: length(spike_list[1])], markershape=:vline, markercolor=:black,
-        xlabel=L"Time(sec)", ylabel=L"The\ Test\ Number", title=L"Raster\ Plot", label=L"The\ First\ Test",dpi=400)
+        xlabel=L"Time(sec)", ylabel=L"The\ Test\ Number", title=L"Raster\ Plot\ For\ the\ First\ Trial",dpi=400, label=nothing)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/PETCH_RasterPlot/Figs/First_RasterPlot_plot.pdf")
 ####First 20 tests
-plot([0,0], [0, 20], color=:red)
+plot([0,0], [0, 20], color=:red, label="Spike Time")
 for i in 1: 20
-    scatter!(spike_list[i],[i for j in 1: length(spike_list[i])], markershape=:vline)
+    scatter!(spike_list[i],[i for j in 1: length(spike_list[i])], markershape=:vline, label=nothing)
 end
-plot!(title=L"Raster\ Plot\ (First\ 20\ Tests)", xlabel=L"Time\ (sec)",ylabel=L"The\ Test\ Number", legend=false, dpi=400)
+plot!(title=L"Raster\ Plot\ (First\ 20\ Tests)", xlabel=L"Time\ (sec)",ylabel=L"The\ Test\ Number", dpi=400)
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/PETCH_RasterPlot/Figs/f20_RasterPlot_plot.pdf")
 close(file)

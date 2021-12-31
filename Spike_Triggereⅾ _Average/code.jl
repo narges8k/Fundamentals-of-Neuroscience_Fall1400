047ae84699d315b8422b3952efa8d6ba1e08e361
Narges <m.dabiri48@gmail.com>
2021-12-31 19:08:56 +0330

    Finished

--- Spike_Triggereⅾ _Average/code.jl
+++ Spike_Triggereⅾ _Average/code.jl
@@ -10,13 +10,24 @@ plot(collect(range(0.0, 1.0, length=2000)),vars["Stim"][1:2000], title=L"Stimulu
 savefig("../../Fundamentals-of-Neuroscience_Fall1400/Spike_Triggereⅾ _Average/Figs/First_sec_signal_plot.pdf")
 
 #######B
-rand_spike=rand(vars["Spike_times"],20)
+spike_time_list=[]
+spike_num_list=[]
+for i in 1:20
+    spike_num=rand(1:598)
+    push!(spike_num_list, spike_num)
+    push!(spike_time_list, vars["Spike_times"][spike_num])
+end
+spike_time_list
+spike_num_list
+num=1  
 plots=[]
-for i in rand_spike
+
+for i in spike_time_list
     y=vars["Stim"][floor(Int, (i-0.075)*2000):floor(Int, i*2000)]
     plt=begin
-        plot(collect(range(i-0.075, i, length=length(y))),y, title=L"Spike\ time\ :%$i",markersize=3,markerstrokewidth = 0.1)
+        plot(collect(range(i-0.075, i, length=length(y))),y,markersize=3, title=L"Spike\ Number: %$(spike_num_list[num])",markerstrokewidth = 0.1)      
     end
+    num+=1
     push!(plots, plt)
 end
 plot(plots...,size = (1000,1000),xlabel=L"Time(s)", ylabel=L"Voltage(mV)", legend=false)
@@ -53,7 +64,7 @@ savefig("../../Fundamentals-of-Neuroscience_Fall1400/Spike_Triggereⅾ _Average/
 #######D
 
 stim20=[]
-for i in rand_spike
+for i in spike_time_list
     push!(stim20,vars["Stim"][floor(Int, (i-0.075)*2000):floor(Int, i*2000)])
 end
 avglist20=[]
@@ -65,7 +76,7 @@ for i in 1:150
     push!(avglist20, mean(each))
 end
 plot()
-for i in rand_spike
+for i in spike_time_list
     plot!(collect(range(-0.075, 0.0, length=150)),vars["Stim"][floor(Int, (i-0.075)*2000):floor(Int, i*2000)][1:150], label=nothing, color=:cornflowerblue)
 end
 plot!(collect(range(-0.075, 0.0, length=150)),avglist20, color=:red, linewidth=5, label="STA", title=L"", xlabel=L"Voltage(mV)", ylabel=L"Time\ Before\ Spike(s)")
